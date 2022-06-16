0e230e3fce4e41f39c45bc3e53e3d2d076e831c4
wazolee <wazolee@gmail.com>
2022-06-16 23:57:25 +0200

    Completed module

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -47,7 +47,7 @@ namespace WishList.Controllers
                     }
                     return View("Register", registerViewModel);
                 }
-                return RedirectToAction("Index", "HomeController");
+                return RedirectToAction("Index", "Home");
             }
             else
             {
